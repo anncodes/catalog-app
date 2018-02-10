9364e9eeb943565dd830695198f101aae9a6e744
Ann Gepulle <annm.gepulle@gmail.com>
2018-02-10 17:44:13 +0100

    Initial commit

---
+++ catalogdata.py
@@ -0,0 +1,98 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import datetime
+from database_setup import *
+
+# To create engine function, let program know what
+# database to connect to and bind the engine
+# to the database
+engine = create_engine('sqlite:///catalogplaces.db')
+Base.metadata.bind = engine
+
+# Create session maker
+DBsession = sessionmaker(bind=engine)
+session = DBsession()
+
+# Add users
+User1 = User(
+    name="Sophie Durand",
+    email="sdurand512@gmail.com",
+    picture="https://placeimg.com/200/200/animals")
+session.add(User1)
+session.commit()
+
+User2 = User(
+    name="Nao Wonders",
+    email="nwonders13@gmail.com",
+    picture="https://placeimg.com/200/200/animals")
+session.add(User2)
+session.commit()
+
+# Create categories
+Category1 = Category(
+    name="Cultural",
+    user_id=1)
+session.add(Category1)
+session.commit()
+
+Category2 = Category(
+    name="Food",
+    user_id=1)
+session.add(Category2)
+session.commit()
+
+Category3 = Category(
+    name="Sports",
+    user_id=2)
+session.add(Category3)
+session.commit()
+
+# Add item places to categories
+ItemPlace1 = ItemPlace(
+    name="Shuri Castle",
+    address="Shuri, Okinawa",
+    description="Shuri Castle is a Ryukyuan gusuku \
+    in Shuri, Okinawa.",
+    photo="http://bit.ly/2EhVDZK",
+    category_id=1,
+    user_id=1)
+session.add(ItemPlace1)
+session.commit()
+
+ItemPlace2 = ItemPlace(
+    name="Makishi Market",
+    address="Naha, Okinawa",
+    description="An excellent place for visitors to go to learn \
+    about everyday life in Okinawa",
+    photo="http://bit.ly/2EhGDeg",
+    category_id=1,
+    user_id=2)
+session.add(ItemPlace2)
+session.commit()
+
+ItemPlace3 = ItemPlace(
+    name="Sefa Utaki",
+    address="Naha, Okinawa",
+    description=" Sefa Utaki is an historical sacred space, \
+    overlooking Kudaka Island, that served as one of the key  \
+    locations of worship in the native religion of the \
+    Ryukyuan people for millennia.",
+    photo="http://bit.ly/2D6WJs5",
+    category_id=1,
+    user_id=1)
+session.add(ItemPlace3)
+session.commit()
+
+ItemPlace4 = ItemPlace(
+    name="Ugan",
+    address="Zamami, Kerama",
+    description="There are a current around Ugan that \
+    standing on the ocean, a dynamic drop off \
+    and schools of fish.",
+    photo="http://bit.ly/2D53Jpf",
+    category_id=2,
+    user_id=1)
+session.add(ItemPlace4)
+session.commit()
+
+print 'Database has been populated'
---
+++ database_setup.py
@@ -0,0 +1,58 @@
+import sys
+import os
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    picture = Column(String(250))
+
+
+class Category(Base):
+    __tablename__ = 'category'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
+    @property
+    def serialize(self):
+        return {
+            'name': self.name,
+            'id': self.id,
+            }
+
+class ItemPlace(Base):
+    __tablename__ = 'item_place'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    address = Column(String(250), nullable=False)
+    description = Column(String(250))
+    photo = Column(String(250))
+    category_id = Column(Integer, ForeignKey('category.id'))
+    category = relationship(Category)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
+    @property
+    def serialize(self):
+        return {
+            'name': self.name,
+            'description': self.description,
+            'id': self.id,
+            'photo': self.photo,
+            }
+
+engine = create_engine('sqlite:///catalogplaces.db')
+Base.metadata.create_all(engine)
