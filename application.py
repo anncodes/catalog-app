251c4e5c3aaa16b45706eec7c842068ace5cb84b
Ann Gepulle <annm.gepulle@gmail.com>
2018-05-17 21:33:53 +0200

    Delete context process from json apis

--- application.py
+++ application.py
@@ -431,19 +431,6 @@ def placeJSON(catalog_name, place_name):
     return jsonify(place=[place.serialize])
 
 
-# @app.context_process
-# def override_url_for():
-#     return dict(url_for=dated_url_for)
-
-# def dated_url_for(endpoint, **values):
-#     if endpoint == 'static':
-#         filename = values.get('filename', None)
-#         if filename:
-#             file_path = os.path.join(app.root_path, endpoint, filename)
-#             values['q'] = int(os.stat(file_path).st_mtime)
-#     return url_for(endpoint, **values)
-
-
 if __name__ == '__main__':
     app.debug = True
     app.secret_key = "altered_secret_key"
